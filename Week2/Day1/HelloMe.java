63b2fadb01bd31d4c3c0bc96a1c0116c34a3b0bf
kmikula <mikula_klara@yahoo.fr>
2018-04-11 19:07:17 +0200

    HelloMe

---
+++ Week2/Day1/HelloMe.java
@@ -0,0 +1,14 @@
+public class HelloMe {
+
+    public static void main (String[] args) {
+        System.out.println("Hello Klari");
+
+        System.out.println("Humpty Dumpty had a great fall.");
+        System.out.println("Humpty Dumpty sat on a wall,");
+        System.out.println("All the king's horses and all the king's men");
+        System.out.println("Couldn't put Humpty together again.");
+
+        System.out.println("Hello Niko");
+
+    }
+}
