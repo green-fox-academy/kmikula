46d852459d031d0a20a62d84bc75cbe1c4c9b095
kmikula <mikula_klara@yahoo.fr>
2018-04-12 22:47:11 +0200

    FavoritNumber

---
+++ Week2/Day1/FavoriteNumber.java
@@ -0,0 +1,9 @@
+public class FavoriteNumber {
+
+    public static void main (String[] args) {
+
+     int favoriteNumber = 2;
+        System.out.println("My favourite number is "+favoriteNumber);
+
+    }
+}
