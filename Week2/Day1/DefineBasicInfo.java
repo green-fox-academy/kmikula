b513b478cd3ce78bae92feeaaf8c89559e84f749
kmikula <mikula_klara@yahoo.fr>
2018-04-12 22:46:06 +0200

    DefineBasicInfo

---
+++ Week2/Day1/DefineBasicInfo.java
@@ -0,0 +1,19 @@
+public class DefineBasicInfo {
+    public static void main (String []args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+
+        String name = "Klári";
+        int age = 35;
+        double height = 1.64;
+        boolean ismarried = false;
+
+        System.out.println("My name is "+ name);
+        System.out.println("I am " + age+ " years old.");
+        System.out.println("I am + " + height+ " meter tall.");
+        System.out.println("Am I married? " + ismarried);
+    }
+}
