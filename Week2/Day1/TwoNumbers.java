2e8b26e90d156740b40f53f30acbee27970390ca
kmikula <mikula_klara@yahoo.fr>
2018-04-12 22:54:38 +0200

    TwoNumbers

---
+++ Week2/Day1/TwoNumbers.java
@@ -0,0 +1,15 @@
+public class TwoNumbers {
+
+    public static void main (String[] args) {
+    int a = 13;
+    int b = 22;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(b * a);
+
+        float c = b / a;
+        System.out.println(c);
+        System.out.println(b % a);
+    }
+}
