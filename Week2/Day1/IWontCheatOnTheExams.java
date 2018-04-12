f52b90e3b8c9a03d16d406305eddee7c31fec827
kmikula <mikula_klara@yahoo.fr>
2018-04-12 22:49:31 +0200

    IWontCheatOnTheExams

---
+++ Week2/Day1/IWontCheatOnTheExams.java
@@ -0,0 +1,13 @@
+public class IWontCheatOnTheExams {
+
+    public static void main (String []args) {
+
+        // Create a program that writes this line 100 times:
+        // "I won't cheat on the exam!"
+
+        for(int i =0; i <= 100; i++) {
+
+            System.out.println( i +" I won't cheat on the exam!");
+        }
+    }
+}
