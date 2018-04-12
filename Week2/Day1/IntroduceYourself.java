3b712917d4a2af964fcebde308c5d1d8c6837fb8
kmikula <mikula_klara@yahoo.fr>
2018-04-12 22:48:44 +0200

    IntroduceYourself

---
+++ Week2/Day1/IntroduceYourself.java
@@ -0,0 +1,8 @@
+public class IntroduceYourself {
+
+    public static void main (String[] args) {
+        System.out.println("Mikula Klára");
+        System.out.println(36);
+        System.out.println("I speak French");
+    }
+}
