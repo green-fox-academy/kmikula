e65b7de909c984cdbb7ef1cb3f3a8d2d855c3962
kmikula <mikula_klara@yahoo.fr>
2018-05-09 20:12:06 +0200

    Clonable

---
+++ Week6/Day2/Clonable/src/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+
+    public static void main(String[] args) {
+
+        Student student = new Student();
+
+        Student student2 = student.clone();
+
+        System.out.println("First student's name is: "+student.name + " , second student's name is: " + student2.name);
+    }
+
+}
