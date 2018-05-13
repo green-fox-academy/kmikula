888c5f64704d85f225bdb0a13e2500994d34444e
kmikula <mikula_klara@yahoo.fr>
2018-05-13 11:31:17 +0200

    Printalbe IF

---
+++ Week6/Day2/Printable/src/Dominoes.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Dominoes {
+    public static void main(String[] args) {
+        ArrayList<Domino> dominoes = initializeDominoes();
+        // You have the list of Dominoes
+        // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
+        // eg: [2, 4], [4, 3], [3, 5] ...
+
+        for (Domino d : dominoes) {
+            d.printAllFields();
+        }
+    }
+
+    static ArrayList<Domino> initializeDominoes() {
+        ArrayList<Domino> dominoes = new ArrayList<>();
+        dominoes.add(new Domino(5, 2));
+        dominoes.add(new Domino(4, 6));
+        dominoes.add(new Domino(1, 5));
+        dominoes.add(new Domino(6, 7));
+        dominoes.add(new Domino(2, 4));
+        dominoes.add(new Domino(7, 1));
+        return dominoes;
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
