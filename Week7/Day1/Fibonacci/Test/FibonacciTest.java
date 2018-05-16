25fab67762c7fb4ed7a49cf8ef065d6200c1910a
kmikula <mikula_klara@yahoo.fr>
2018-05-16 20:30:41 +0200

    Fibonacci

--- Week7/Day1/Fibonacci/Test/FibonacciTest.java
+++ Week7/Day1/Fibonacci/Test/FibonacciTest.java
@@ -8,16 +8,38 @@ public class FibonacciTest {
 
     @Test
     public void testZeroFibonacci() {
-        int expectedValue = 0;
-        int currentValue = fibo.fibonacci(0);
+        long expectedValue = 0;
+        long currentValue = fibo.fibonacci(0);
         assertEquals(expectedValue, currentValue);
     }
 
     @Test
     public void fibonacciTest() {
-        int expectedValue = 6765;
-        int currentValue = fibo.fibonacci(20);
+        long expectedValue = 6765;
+        long currentValue = fibo.fibonacci(20);
         assertEquals(expectedValue, currentValue);
     }
+
+    @Test
+    public void fibonacciTest2() {
+        long expectedValue = 13;
+        long currentValue = fibo.fibonacci(7);
+        assertEquals(expectedValue, currentValue);
+    }
+
+    @Test
+    public void fibonacciTest3() {
+        long expectedValue = 1;
+        long currentValue = fibo.fibonacci(2);
+        assertEquals(expectedValue, currentValue);
+    }
+
+    @Test
+    public void fibonacciTest4() {
+        long expectedValue = 5702887;
+        long currentValue = fibo.fibonacci(34);
+        assertEquals(expectedValue, currentValue);
+    }
+
 
 }
--- Week7/Day1/Fibonacci/src/Fibonacci.java
+++ Week7/Day1/Fibonacci/src/Fibonacci.java
@@ -2,23 +2,23 @@ import java.util.ArrayList;
 
 public class Fibonacci {
 
-    ArrayList<Integer> fibonacciNrs;
+    ArrayList<Long> fibonacciNrs;
 
     public Fibonacci() {
         this.fibonacciNrs = new ArrayList<>();
     }
 
-    public int fibonacci(int index) {
+    public long fibonacci(int index) {
 
 
-        fibonacciNrs.add(0);
-        fibonacciNrs.add(1);
+        fibonacciNrs.add((long)0);
+        fibonacciNrs.add((long)1);
 
         int k = 2;
 
         while (fibonacciNrs.size() <= (index+1) ) {
 
-            int sumOfPreviousTwo = fibonacciNrs.get(k - 2) + fibonacciNrs.get(k - 1);
+            Long sumOfPreviousTwo = Long.valueOf(fibonacciNrs.get(k - 2) + fibonacciNrs.get(k - 1));
             fibonacciNrs.add(sumOfPreviousTwo);
             k++;
 
