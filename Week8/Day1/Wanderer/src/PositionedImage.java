67bb3a2a7b4382bb07c3351266dc80cf0a2ac465
kmikula <mikula_klara@yahoo.fr>
2018-05-20 13:36:35 +0200

    10*10 floor

---
+++ Week8/Day1/Wanderer/src/PositionedImage.java
@@ -0,0 +1,28 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class PositionedImage {
+
+    BufferedImage image;
+    int posX, posY;
+
+    public PositionedImage(String filename, int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+        try {
+            image = ImageIO.read(new File(filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void draw(Graphics graphics) {
+        if (image != null) {
+            graphics.drawImage(image, posX, posY, null);
+        }
+    }
+}
