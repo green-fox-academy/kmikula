eb7777186a108f55605a5f438d056e2137405b50
kmikula <mikula_klara@yahoo.fr>
2018-05-23 20:23:57 +0200

    hero direction works

--- Week8/Day1/Wanderer/src/Board.java
+++ Week8/Day1/Wanderer/src/Board.java
@@ -18,12 +18,14 @@ public class Board extends JComponent implements KeyListener {
 
 
     PositionedImage hero = new PositionedImage("hero-down.png", 0, 0);
-
+//    PositionedImage hero2 = new PositionedImage("hero-up.png", 0, 0);
+//    PositionedImage hero3 = new PositionedImage("hero-left.png", 0, 0);
+//    PositionedImage hero4 = new PositionedImage("hero-right.png", 0, 0);
 
     public Board() {
 
-        testBoxX = 0;     //300
-        testBoxY = 0;     //300
+        testBoxX = 0;
+        testBoxY = 0;
 
         // set the size of your draw board
         setPreferredSize(new Dimension(720, 720));
@@ -100,13 +102,29 @@ public class Board extends JComponent implements KeyListener {
     public void keyReleased(KeyEvent e) {
         // When the up or down keys hit, we change the position of our box
         if (e.getKeyCode() == KeyEvent.VK_UP) {
+
             hero.posY -= 72;
+
+          // hero.posY =testBoxY- 72;
+          hero = new PositionedImage("src/hero-up.png",hero.posX, hero.posY);
+
         } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+
+
             hero.posY += 72;
+           hero = new PositionedImage("src/hero-down.png", hero.posX, hero.posY);
+
         } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+
+
             hero.posX -= 72;
+            hero = new PositionedImage("src/hero-left.png", hero.posX, hero.posY);
         } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+
+
             hero.posX += 72;
+            hero = new PositionedImage("src/hero-right.png", hero.posX, hero.posY);
+
         }
         // and redraw to have a new picture with the new coordinates
         repaint();
