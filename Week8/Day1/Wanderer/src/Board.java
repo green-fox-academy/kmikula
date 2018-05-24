ebf491cc37ed5b00cba29308a14f7cc9a358baf4
kmikula <mikula_klara@yahoo.fr>
2018-05-25 00:00:58 +0200

    hero does not move through the walls

--- Week8/Day1/Wanderer/src/Board.java
+++ Week8/Day1/Wanderer/src/Board.java
@@ -13,8 +13,10 @@ public class Board extends JComponent implements KeyListener {
     int testBoxX;
     int testBoxY;
     String level;
+    public static final int tilePx = 72;
     ArrayList<String> mapTemplate;
-    int[][] map = drawMap();
+
+    ArrayList<ArrayList<Character>> map = drawMap();
 
 
     PositionedImage hero = new PositionedImage("hero-down.png", 0, 0);
@@ -30,10 +32,9 @@ public class Board extends JComponent implements KeyListener {
         setVisible(true);
     }
 
-    public int[][] drawMap() {
-        int[][] coord = new int[10][10];
+
+    public ArrayList<ArrayList<Character>> drawMap() {
         level = "level0.txt";
-        int index2 = 0;
 
         Path filepath = Paths.get(level);
 
@@ -44,14 +45,15 @@ public class Board extends JComponent implements KeyListener {
         }
 
 
-        for (int i = 0; i < mapTemplate.size(); i++) {
-            index2 = 0;
-            for (int j = 0; j < 20; j += 2) {
-                coord[index2][i] = mapTemplate.get(i).charAt(j);
+        ArrayList<ArrayList<Character>> coord = new ArrayList<>();
+        ArrayList<Character> lineInChars;
 
-                index2++;
+        for (String line : mapTemplate) {
+            lineInChars = new ArrayList<>();
+            for (Character actChar : line.toCharArray()) {
+                lineInChars.add(actChar);
             }
-
+            coord.add(lineInChars);
         }
         return coord;
     }
@@ -62,18 +64,18 @@ public class Board extends JComponent implements KeyListener {
         super.paint(graphics);
         int indexX = 0;
         int indexY = 0;
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map.length; j++) {
-                if (map[i][j] == 48) {
-                    indexX = i * 72;
-                    indexY = j * 72;
-                    graphics.fillRect(indexX, indexY, 72, 72);
+        for (int i = 0; i < map.size(); i++) {
+            for (int j = 0; j < map.get(i).size(); j++) {
+                if (map.get(i).get(j) == '0') {
+                    indexX = i * tilePx;
+                    indexY = j * tilePx;
+                    graphics.fillRect(indexX, indexY, tilePx, tilePx);
                     PositionedImage image = new PositionedImage("wall.png", indexX, indexY);
                     image.draw(graphics);
-                } else if (map[i][j] == 49) {
-                    indexX = i * 72;
-                    indexY = j * 72;
-                    graphics.fillRect(indexX, indexY, 72, 72);
+                } else if (map.get(i).get(j) == '1') {
+                    indexX = i * tilePx;
+                    indexY = j * tilePx;
+                    graphics.fillRect(indexX, indexY, tilePx, tilePx);
                     PositionedImage image = new PositionedImage("floor.gif", indexX, indexY);
                     image.draw(graphics);
                 }
@@ -99,10 +101,20 @@ public class Board extends JComponent implements KeyListener {
     @Override
     public void keyReleased(KeyEvent e) {
         // When the up or down keys hit, we change the position of our box
+
         if (e.getKeyCode() == KeyEvent.VK_UP) {
-            if (hero.posY >= 72) {
-                hero.posY -= 72;
-                hero = new PositionedImage("src/hero-up.png", hero.posX, hero.posY);
+
+            if (hero.posY >= tilePx) {
+
+                if ((map.get(hero.posX / tilePx).get((hero.posY - tilePx) / tilePx) == '0')) {
+                    hero.posY += 0;
+                    hero = new PositionedImage("src/hero-up.png", hero.posX, hero.posY);
+
+                } else {
+                    hero.posY -= tilePx;
+                    hero = new PositionedImage("src/hero-up.png", hero.posX, hero.posY);
+
+                }
             } else {
                 hero.posY += 0;
                 hero = new PositionedImage("src/hero-up.png", hero.posX, hero.posY);
@@ -110,9 +122,15 @@ public class Board extends JComponent implements KeyListener {
 
         } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 
-            if (hero.posY < 648) {
-                hero.posY += 72;
-                hero = new PositionedImage("src/hero-down.png", hero.posX, hero.posY);
+            if (hero.posY < 720 - tilePx) {
+                if ((map.get(hero.posX / tilePx).get((hero.posY + tilePx) / tilePx) == '0')) {
+                    hero.posY += 0;
+                    hero = new PositionedImage("src/hero-down.png", hero.posX, hero.posY);
+
+                } else {
+                    hero.posY += tilePx;
+                    hero = new PositionedImage("src/hero-down.png", hero.posX, hero.posY);
+                }
             } else {
                 hero.posY += 0;
                 hero = new PositionedImage("src/hero-down.png", hero.posX, hero.posY);
@@ -120,25 +138,37 @@ public class Board extends JComponent implements KeyListener {
 
         } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
 
-            if (hero.posX >= 72) {
-                hero.posX -= 72;
-                hero = new PositionedImage("src/hero-left.png", hero.posX, hero.posY);
+            if (hero.posX >= tilePx) {
+                if ((map.get((hero.posX - tilePx) / tilePx).get(hero.posY / tilePx) == '0')) {
+                    hero.posX += 0;
+                    hero = new PositionedImage("src/hero-left.png", hero.posX, hero.posY);
+
+                } else {
+                    hero.posX -= tilePx;
+                    hero = new PositionedImage("src/hero-left.png", hero.posX, hero.posY);
+                }
             } else {
                 hero.posX -= 0;
                 hero = new PositionedImage("src/hero-left.png", hero.posX, hero.posY);
             }
         } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
 
-            if (hero.posX < 648) {
-                hero.posX += 72;
-                hero = new PositionedImage("src/hero-right.png", hero.posX, hero.posY);
+            if (hero.posX < 720 - tilePx) {
+                if ((map.get((hero.posX + tilePx) / tilePx).get(hero.posY / tilePx) == '0')) {
+                    hero.posY += 0;
+                    hero = new PositionedImage("src/hero-right.png", hero.posX, hero.posY);
+
+                } else {
+                    hero.posX += tilePx;
+                    hero = new PositionedImage("src/hero-right.png", hero.posX, hero.posY);
+                }
             } else {
                 hero.posX += 0;
                 hero = new PositionedImage("src/hero-right.png", hero.posX, hero.posY);
             }
-
         }
         // and redraw to have a new picture with the new coordinates
         repaint();
     }
+
 }
